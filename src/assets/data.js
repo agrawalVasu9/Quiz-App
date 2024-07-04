103cae77c99244d7f7c1ebdf14b34931154ec2b2
agrawalVasu9 <agrawal50vasu@gmail.com>
2024-07-04 12:15:27 +0530

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,36 @@
+export const data = [{
+    question: "A flashing red traffic light signifies that a driver should do what?",
+    option1: "stop",
+    option2: "speed up",
+    option3: "proceed with caution",
+    option4: "honk the horn",
+    ans: "1"
+  }, {
+    question: "A pita is a type of what?",
+    option1: "fresh fruit",
+    option2: "flat bread",
+    option3: "French tart",
+    option4: "friend bean dip",
+    ans: "2"
+  }, {
+    question: "According to a Yale University study, what smell is the most recognizable to American adults?",
+    option1: "tuna",
+    option2: "laundry",
+    option3: "popcorn",
+    option4: "coffee",
+    ans: "1"
+  }, {
+    question: "According to the popular saying, what should you do 'when in Rome'?",
+    option1: "watch your wallet",
+    option2: "see the Coliseum",
+    option3: "as the Romans do",
+    option4: "don't drink the water",
+    ans: "3"
+  }, {
+    question: "At what age can someone first attend an R-rated movie without an accompanying adult?",
+    option1: "15",
+    option2: "17",
+    option3: "18",
+    option4: "21",
+    ans: "3"
+  }]
